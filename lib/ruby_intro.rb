86e446e59fef989cae9e6b5e2b351d8450e498ce
vkhanna92 <43229822+vkhanna92@users.noreply.github.com>
2018-09-13 22:10:11 -0500

    homework changes

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,64 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+        sum1 = 0
+        for x in arr
+                 sum1 = sum1+x
+        end
+        sum1
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+        if arr.length == 0
+                return 0
+        elsif arr.length == 1
+                return arr[0]
+        else
+                return arr.max(2).reduce(:+)
+        end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+        hash = Hash.new
+        for i in arr
+                if hash.key?(n-i)
+                        return true
+                end
+                hash[i] = i
+        end
+        return false
 end
 
-# Part 2
+#Homework2
 
 def hello(name)
-  # YOUR CODE HERE
+        "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+        s =~ /^(?=[^aeiou])(?=[a-z])/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+        return true if s == "0"
+        /^[10]*00$/.match(s) != nil
 end
 
-# Part 3
-
+# Homework3
 class BookInStock
-# YOUR CODE HERE
+        attr_accessor :isbn, :price
+        #Constructor
+		
+        def initialize(isbn,price)
+                if isbn == "" or  price <= 0
+                        raise ArgumentError.new("Invalid parameter values")
+                else
+                        @isbn = isbn
+                        @price = price
+                end
+        end
+        def price_as_string
+                sprintf("$%.2f", @price)
+        end
+
 end
